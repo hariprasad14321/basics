ec0c37af05a2634144243dc9707a18d1427e864d
Gadireddy Satya Hari Prasad Reddy <104205387+hariprasad14321@users.noreply.github.com>
2023-02-22 00:44:57 +0530

    git.push

---
+++ Nikki.java
@@ -0,0 +1,7 @@
+public class Nikki {
+   public static void main(String[]args){
+    String ss = "Hello world";
+    System.out.println(ss);
+   }
+    
+}
---
+++ java/VarDemo.java
@@ -0,0 +1,12 @@
+public class VarDemo {
+    public void sample() {
+        int a = 5 ;
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        VarDemo s = new VarDemo();
+        s.sample();
+
+    }
+
+}
